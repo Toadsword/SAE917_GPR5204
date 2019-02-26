53d1aa3a884d4ac4019a8300ffcfa872ea868040
Duncan Bourquard <bourquardduncan@gmail.com>
2019-02-26 17:44:39 +0100

    Test access tile.

--- tests/test_tilemap.cpp
+++ tests/test_tilemap.cpp
@@ -30,7 +30,7 @@ static void BM_LoadTilemapNaive(benchmark::State& state) {
 		benchmark::DoNotOptimize(initTilemapNaive(state.range(0)));
 	}
 }
-BENCHMARK(BM_LoadTilemapNaive)->Range(fromRange, toRange);
+//BENCHMARK(BM_LoadTilemapNaive)->Range(fromRange, toRange);
 
 std::vector<unsigned> initTilemapOpti(const size_t n)
 {
@@ -45,6 +45,37 @@ static void BM_LoadTilemap(benchmark::State& state) {
 		benchmark::DoNotOptimize(initTilemapOpti(state.range(0)));
 	}
 }
-BENCHMARK(BM_LoadTilemap)->Range(fromRange, toRange);
+//BENCHMARK(BM_LoadTilemap)->Range(fromRange, toRange);
+
+
+
+unsigned getTileAtNaive(std::vector<std::vector<unsigned>>& tiles, const int n, const int m)
+{
+	return tiles[n][m];
+}
+
+static void BM_GetTileAtNaive(benchmark::State& state) {
+	std::vector<std::vector<unsigned>> tiles = std::vector<std::vector<unsigned>>{ 10000 , std::vector<unsigned>(10000) };
+	for (auto _ : state)
+	{
+		benchmark::DoNotOptimize(getTileAtNaive(tiles, state.range(0) / 2, state.range(0) / 2));
+	}
+}
+BENCHMARK(BM_GetTileAtNaive)->Range(fromRange, toRange);
+
+unsigned getTileAtOpti(std::vector<unsigned>& tiles, const size_t tilemapSizeY, const int n, const int m)
+{
+	return tiles.at(n * tilemapSizeY + m);
+}
+
+static void BM_GetTileAtOpti(benchmark::State& state) {
+	std::vector<unsigned> tiles = std::vector<unsigned>(10000 * 1000, 0);
+	for (auto _ : state)
+	{
+		benchmark::DoNotOptimize(getTileAtOpti(tiles, state.range(0), state.range(0) / 2, state.range(0) / 2));
+	}
+}
+BENCHMARK(BM_GetTileAtOpti)->Range(fromRange, toRange);
 
 BENCHMARK_MAIN();
+
